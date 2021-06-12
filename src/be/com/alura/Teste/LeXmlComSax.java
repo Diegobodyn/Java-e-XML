44e380e591416b8077fb89724c3a0e2d5f40eb33
DiegoBodyn <diegosilvadhs@gmail.com>
2021-06-12 13:57:21 -0300

    Primeiro commit

---
+++ src/be/com/alura/Teste/LeXmlComSax.java
@@ -0,0 +1,35 @@
+package be.com.alura.Teste;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream.GetField;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import be.com.alura.Handlers.ProdutosHendler;
+
+//Necessário um Hendler que faz todo a lógica.
+
+public class LeXmlComSax {
+	public static void main(String[] args) throws Exception {
+		
+		//Usa o XMLReader
+		XMLReader leitor = XMLReaderFactory.createXMLReader();
+		//Necessita de um ProdutosHendler
+		ProdutosHendler logica = new ProdutosHendler();
+		//Atribui o valor encontrado no ProdutosHendler
+		leitor.setContentHandler(logica);
+		//Acessa o arquivo .xml
+		InputStream ips = new FileInputStream("src/vendas.xml");
+		//Lê o arquivo .xml
+		InputSource is = new InputSource(ips);
+		leitor.parse(is);
+		
+		System.out.println(logica.getProdutos());
+	}
+}
---
+++ src/be/com/alura/Teste/LeXmlStax.java
@@ -0,0 +1,62 @@
+package be.com.alura.Teste;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
+
+import be.com.alura.Model.Produto;
+
+//Trabalha com eventos, pegando somente oq precisa
+
+public class LeXmlStax {
+	public static void main(String[] args) throws Exception {
+		InputStream is = new FileInputStream("src/vendas.xml"); //Imputando o arquivo
+		XMLInputFactory factory = XMLInputFactory.newInstance(); //Fábrica que consegue a instância de um xml
+		XMLEventReader eventos = factory.createXMLEventReader(is); //Responsável por eventos
+		List<Produto> produtos = new ArrayList<>(); //Coleção lista de produtos
+		
+		//Percorre os eventos
+		while (eventos.hasNext()) {
+			//pega proximo produto
+			XMLEvent evento = eventos.nextEvent();
+			//se for um produto cria o produto
+			if (evento.isStartElement() && evento.asStartElement().getName().getLocalPart().equals("produto")) {
+				Produto produto = criaUmProduto(eventos); //Invoca método passando o evento
+				produtos.add(produto); //Add um produto
+			}
+		}
+		System.out.println(produtos); //imprime os produtos encontrados no .xml
+	}
+	
+	private static Produto criaUmProduto(XMLEventReader eventos) throws Exception {
+		Produto produto = new Produto();
+		
+		//Percorre os eventos
+		while (eventos.hasNext()) {
+			//pega proximo evento
+			XMLEvent evento = eventos.nextEvent();
+			//se for nome captura e seta no produto
+			if (evento.isStartElement() && evento.asStartElement().getName().getLocalPart().equals("nome")) {
+				evento = eventos.nextEvent();
+				String nome = evento.asCharacters().getData();
+				produto.setNome(nome);
+			//se for preco captura e seta no produto	
+			} else if (evento.isStartElement() && evento.asStartElement().getName().getLocalPart().equals("preco")) {
+				evento = eventos.nextEvent();
+				String conteudo = evento.asCharacters().getData();
+				double preco = Double.parseDouble(conteudo);
+				produto.setPreco(preco);
+				//se for produto parar o laço	
+			} else if (evento.isEndElement() && evento.asEndElement().getName().getLocalPart().equals("produto")) {
+				break;
+			}
+		}
+		return produto;
+	}
+}
